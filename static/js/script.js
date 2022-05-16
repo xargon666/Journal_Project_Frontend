353b5107470914ae11fd95b371204fa9700a625f
“millman97” <thomas.millington@mail.com>
2022-05-16 12:46:24 +0100

    fix new post button on mobile

---
+++ static/js/script.js
@@ -0,0 +1,9 @@
+const newPostBtn = document.getElementById("newPostBtnDiv");
+
+newPostBtn.addEventListener('click', (e) => {
+    document.getElementById("createPost").style.display = 'block';
+});
+
+function createNewPost(){
+    return 2;
+}
